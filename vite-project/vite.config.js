e43ce8dfe82a8bafea98016589ff20f0d1711dbc
Fredrick Muikia <thuofred1@gmail.com>
2024-12-04 03:11:34 -0500

    Fixed deploy

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [react()],
-    base: '/job-application-helper/', // Updated to the correct repository name
-    build: {
-        outDir: 'dist', // Ensure the build directory is correct
-    },
+    base: 'https://github.com/fredthuo1/job-application-helper/vite-project'
 });
